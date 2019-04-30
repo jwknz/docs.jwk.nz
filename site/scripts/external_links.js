3971aef2af78db449e00e6032e71629fd191f61c
Jeffrey Kranenburg <jeff_mba@MBA.local>
2019-04-30 14:05:01 +1200

    Updated Site

---
+++ site/scripts/external_links.js
@@ -0,0 +1,7 @@
+var anchors = document.querySelectorAll('a');
+
+for (var i = 0; i < anchors.length; i++) {
+	if (anchors[i].host !== window.location.hostname) {
+		anchors[i].setAttribute('target', '_blank');
+ 	}
+}

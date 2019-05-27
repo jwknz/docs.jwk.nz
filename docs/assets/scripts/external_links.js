d9b342e17fde2774b9a9dce8eb9f8679fc1ede7f
Jeffrey Kranenburg <jwkranenburg@icloud.com>
2019-05-27 21:18:28 +1200

    js??

--- docs/assets/scripts/external_links.js
+++ docs/assets/scripts/external_links.js
@@ -1,7 +1,9 @@
-var anchors = document.querySelectorAll('a');
+// var anchors = document.querySelectorAll('a');
 
-for (var i = 0; i < anchors.length; i++) {
-	if (anchors[i].host !== window.location.hostname) {
-		anchors[i].setAttribute('target', '_blank');
- 	}
-}
+// for (var i = 0; i < anchors.length; i++) {
+// 	if (anchors[i].host !== window.location.hostname) {
+// 		anchors[i].setAttribute('target', '_blank');
+//  	}
+// }
+
+console.log("TESTING");
